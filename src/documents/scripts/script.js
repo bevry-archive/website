eef3ceab04d051004cf43055b9711fdf20d61ef4
Benjamin Lupton <b@lupton.cc>
2016-06-14 19:49:06 +0800

    converted script file from coffeescript to javascript

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,84 +1,88 @@
-$window = $(window)
-$document = $(document)
-$body = $(document.body)
-$modalBackdrop = $('.modal.backdrop')
+(function () {
+	var $window = $(window)
+	var $document = $(document)
+	var $body = $(document.body)
+	var $modalBackdrop = $('.modal.backdrop')
+	var googleAnalytics = window._gaq
 
-# Modals
-hideModals = (hash) ->
-	$('.modal').hide()
-	document.location.hash = ''  if hash
+	// Modals
+	function hideModals (hash) {
+		$('.modal').hide()
+		if ( hash ) {
+			document.location.hash = ''
+		}
+	}
 
-$document.on 'keyup', (event) ->
-	if $modalBackdrop.is(':visible') and event.keyCode is 27  # escape
-		hideModals(true)
-
-$modalBackdrop.on 'click', (event) ->
-	event.stopImmediatePropagation()
-	event.preventDefault()
-	hideModals(true)
-
-showPaymentModel = ->
-	_gaq?.push(['_trackEvent', "Payment Modal", document.title, document.location.href, 0, true])
-
-	$modal = $('.payment.modal').css({
-		top: '5.5em'
-		height: 'auto'
-		opacity: 0
-	}).show()
-	$modalBackdrop = $('.modal.backdrop').css({
-		height: window.innerHeight*2
+	$document.on('keyup', function (event) {
+		// 27 is escape key
+		if ( $modalBackdrop.is(':visible') && event.keyCode === 27 ) {
+			hideModals(true)
+		}
 	})
 
-	modalOffset = $modal.offset()
-	if $modal.height()+modalOffset.top*2 > window.innerHeight
-		$modal.css({
-			top: modalOffset.left
-			maxHeight: window.innerHeight-modalOffset.left*2
+	$modalBackdrop.on('click', (event) {
+		event.stopImmediatePropagation()
+		event.preventDefault()
+		hideModals(true)
+	})
+
+	// bank details
+	function showPaymentModel {
+		if ( googleAnalytics ) {
+			googleAnalytics.push([
+				'_trackEvent',
+				'Payment Modal',
+				document.title,
+				document.location.href,
+				0,
+				true
+			])
+		}
+
+		var $modal = $('.payment.modal').css({,
+			top: '5.5em',
+			height: 'auto',
+			opacity: 0
+		}).show()
+
+		var $modalBackdrop = $('.modal.backdrop').css({
+			height: window.innerHeight * 2
 		})
+
+		var modalOffset = $modal.offset()
+		if ( $modal.height() + modalOffset.top * 2 > window.innerHeight ) {
+			$modal.css({
+				top: modalOffset.left,
+				maxHeight: window.innerHeight - modalOffset.left * 2
+			})
+		}
+	}
 
 	$modalBackdrop.show()
 	$modal.css({
 		opacity: 1
 	})
 
-$window.on 'hashchange', ->
-	hash = document.location.hash.replace('#', '')
-
-	switch hash
-		when 'past'
-			$body.removeClass('today future').addClass('past')
-
-		when 'today'
-			$body.removeClass('past future').addClass('today')
-
-		when 'future'
-			$body.removeClass('past today').addClass('future')
-
-	if hash is 'payment'
-		showPaymentModel()
-	else
+	$window.on('hashchange', function () {
+		var hash = document.location.hash.replace('#', '')
 		hideModals()
+		switch ( hash ) {
+			case 'past':
+				$body.removeClass('today future').addClass('past')
+				break
 
-$window.trigger('hashchange')
+			case 'today':
+				$body.removeClass('past future').addClass('today')
+				break
 
-stripeKey = '<%- @stripePublicKey %>'
-$stripeButton = $('#stripe_payment_button')
-if stripeKey
-	stripeHandler = StripeCheckout.configure({
-		key: stripeKey
-		currency: 'USD'
+			case 'future':
+				$body.removeClass('past today').addClass('future')
+				break
+
+			case 'payment':
+				showPaymentModel()
+				break
+		}
+		$window.trigger('hashchange')
 	})
-	$stripeButton.click (e) ->
-		e.preventDefault()
-		amount = prompt('In USD, how much would you like to donate?')
-		amount = String(amount).replace(/\..*$/, '').replace(/[^0-9]/g, '')
-		amount = parseInt(amount, 10)
-		if amount and !isNaN(amount)
-			# Open Checkout with further options
-			stripeHandler.open({
-				name: 'Donation to Bevry',
-				description: 'Support our open-source initiatives',
-				amount: amount*100
-			})
-else
-	$stripeButton.parent().addClass('hidden')
+})()
